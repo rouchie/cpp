d8e222264fa9b861cabca361619370fc04c9473d
rouchie <rouchie@163.com>
2023-04-05 22:09:15 +0800

    s

--- src/vector.cpp
+++ src/vector.cpp
@@ -34,5 +34,23 @@ int main()
         SPDLOG_INFO("size[{}] capacity[{}]", v.size(), v.capacity());
     }
 
+    {
+        std::vector<uint32_t> v(20, 1024);
+        for (auto ii = v.begin(); ii < v.end(); ++ii) {
+            SPDLOG_INFO("[{}]", *ii);
+        }
+
+        //对于容器来说自身带的begin/end函数跟std::begin/std::end函数没有区别
+        //std::begin/std::end是c++11里添加的，可以使用在数组上
+        for (auto ii = std::begin(v); ii < std::end(v); ++ii) {
+            SPDLOG_INFO("[{}]", *ii);
+        }
+
+        const char name[] = "rouchie";        
+        for (auto c = std::begin(name); c < std::end(name); ++c) {
+            SPDLOG_INFO("[{}]", *c);
+        }
+    }
+
     return 0;
 }
