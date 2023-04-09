a6b7c16b99805b365f0ea5cb89b04f9576f634cf
rouchie <rouchie@163.com>
2023-04-09 22:52:05 +0800

    deque

--- 3rdparty/3rdparty.h
+++ 3rdparty/3rdparty.h
@@ -16,6 +16,7 @@
 #include <memory>
 
 #include <vector>
+#include <deque>
 #include <queue>
 #include <list>
 #include <map>
---
+++ src/deque.cpp
@@ -0,0 +1,11 @@
+#include "3rdparty.h"
+
+// deque 和 vector 不同，不是数组的形式存放，操作要比vector慢
+// 提供随机访问迭代器
+
+int main()
+{
+    spdlog_init();
+
+    return 0;
+}
