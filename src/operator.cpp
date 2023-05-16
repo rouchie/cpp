8c109481eed6b9bbbb8a5da0a205a17ef30ecf08
rouchie <rouchie@163.com>
2023-05-16 16:10:59 +0800

    lambda

--- src/operator.cpp
+++ src/operator.cpp
@@ -7,6 +7,18 @@ class A {
             SPDLOG_INFO("i[{}]", i);
             return i;
         }
+
+        // 赋值构造
+        A& operator=(const A & a) {
+            SPDLOG_INFO("operator= const A &");
+            return *this;
+        }
+
+        // 移动赋值
+        A& operator=(A && a) {
+            SPDLOG_INFO("operator= A &&");
+            return *this;
+        }
 };
 
 int main()
