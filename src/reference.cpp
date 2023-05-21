403220e789a7b2cc1becde514c3aae4b97760863
rouchie <rouchie@163.com>
2023-05-21 20:36:41 +0800

    ref

--- src/reference.cpp
+++ src/reference.cpp
@@ -3,6 +3,35 @@
 // 引用必须立刻初始化，不管是左值引用还是右值引用
 // 常量左值引用称为万能引用，可以任何值来初始化
 
+class A {
+    public:
+        A() {
+            SPDLOG_INFO("A");
+        }
+        ~A() {
+            SPDLOG_INFO("~A");
+        }
+        A(const A& a) {
+            SPDLOG_INFO("const A&");
+        }
+        A(A&& a) {
+            SPDLOG_INFO("A&&");
+        }
+        A& operator=(const A& a) {
+            SPDLOG_INFO("operator const A&");
+            return *this;
+        }
+        A& operator=(A&& a) {
+            SPDLOG_INFO("operator A&&");
+            return *this;
+        }
+};
+
+A getA()
+{
+    return A();
+}
+
 int main()
 {
     spdlog_init();
@@ -25,6 +54,10 @@ int main()
     const int && h = 7;
 
     SPDLOG_INFO("i[{}] j[{}] n[{}]", i, j, n);
+
+    A a0 = getA();
+    A && a1 = getA();
+    A a2 = a0;
 
     return 0;
 }
