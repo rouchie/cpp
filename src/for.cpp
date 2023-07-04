b01901afe1c305db8876719b485d429724ad250a
rouchie <rouchie@163.com>
2023-07-04 21:52:40 +0800

    compare

--- src/for.cpp
+++ src/for.cpp
@@ -37,5 +37,20 @@ int main()
         SPDLOG_INFO("{} - {}", v.first, v.second);
     }
 
+    std::vector<int> v {1, 2, 3};
+
+    std::for_each(std::begin(v), std::end(v), [](int & i) {
+        SPDLOG_INFO("value [{}]", i);
+    });
+
+    std::for_each(m.begin(), m.end(), [](const std::pair<int, std::string>& pair) {
+        SPDLOG_INFO("first[{}] second[{}]", pair.first, pair.second);
+    });
+
+    // 下面的写法错误，必须加上const
+    // 因为map的key不可以改动，所以必须带上const
+    // std::for_each(m.begin(), m.end(), [](std::pair<int, std::string>& pair) {});
+
+
     return 0;
 }
